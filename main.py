45daa6db98c920494e5af52e5785e60c0d74594b
Forest Ranger (what is this name for?) <my.forest.ranger@gmail.com>
2023-11-13 18:16:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+def anketa (name, second_name, date_of_birth, month_of_birth, year_of_birth):
+
+    print(f"Мене звати {name}, прізвище {second_name}, я народився {date_of_birth} числа, місяць {month_of_birth} , {year_of_birth} році")
+
+anketa ("Тарас", "Очеретюк", "1", "Січень", "1990")
